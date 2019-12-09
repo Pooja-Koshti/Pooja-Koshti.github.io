8e6a7ed1ee55cffe86eacfbe078283c5e4bd82ce
Pooja Koshti <pooja@example.com>
2019-12-09 11:23:21 +0530

    first commit

---
+++ sw_cached_pages.js
@@ -0,0 +1,30 @@
+const cacheName='v1';
+
+const cacheAssets =[
+    'form.html',
+    'main.js',
+    'details.php'
+];
+
+self.addEventListener('install',e=>{
+    console.log('Service Worker : Installed');
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache=>{
+                console.log('Service Worker:Caching Files');
+                cache.addAll(cacheAssets);
+            })
+            .then(()=> self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate',e=>{
+    console.log("Service Worker:Activated");
+});
+
+self.addEventListener('fetch',e=>{
+    console.log('Service fetching');
+    e.respondWith(fetch(e.request).catch(()=>caches.watch(e.request)));
+});
